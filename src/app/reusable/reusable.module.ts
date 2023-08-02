4f803ab2e17e79a92faa1cedb3224942354aed12
zrodrigolimaz <zrodrigolimaz@gmail.com>
2023-08-02 17:20:11 -0300

    Create CardList and SearchBar components with custom styles

--- src/app/reusable/reusable.module.ts
+++ src/app/reusable/reusable.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeaderbarComponent } from './headerbar/headerbar.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
+import { ListComponent } from './list/list.component';
 
 
 @NgModule({
   declarations: [
-    HeaderbarComponent
+    HeaderbarComponent,
+    SearchbarComponent,
+    ListComponent
   ],
   exports: [
-    HeaderbarComponent
+    HeaderbarComponent,
+    SearchbarComponent,
+    ListComponent
   ],
   imports: [
     CommonModule
