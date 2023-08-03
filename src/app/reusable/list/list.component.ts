f800938e5aa371d648ce58d3041dceaf3bfff6e7
zrodrigolimaz <zrodrigolimaz@gmail.com>
2023-08-02 23:03:23 -0300

    Updated app module and list component, added new pokemon api service

--- src/app/reusable/list/list.component.ts
+++ src/app/reusable/list/list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { PokeapiService } from 'src/app/services/pokeapi.service';
 
 @Component({
   selector: 'list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent {
+export class ListComponent implements OnInit, OnDestroy {
+  serviceSubscription = new Subscription
+  constructor(private pokeapiService: PokeapiService) { }
 
+  ngOnInit(): void {
+    // Coloque aqui o código que você quer que seja executado quando o componente for inicializado
+    this.serviceSubscription = this.pokeapiService.apiListAllPokemons.subscribe(
+      res => res
+    );
+  }
+  ngOnDestroy(): void {
+    this.serviceSubscription.unsubscribe();
+  }
 }
---
+++ src/app/services/pokeapi.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+// Observable
+import { Observable } from 'rxjs';
+import { map, tap } from "rxjs/operators";
+
+interface Pokemon {
+  name: string;
+  url: string;
+  details?: PokemonDetails;
+}
+
+interface PokemonList {
+  count: number;
+  next: string;
+  previous: string;
+  results: Pokemon[];
+}
+
+interface PokemonDetails {
+  // pegar os dados dos detalhes de cada pokemon que eu vou querer filtrar para popular no details 
+  // por exemplo:
+  // name: string;
+  // weight: number;
+  // etc.
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokeapiService {
+
+  private url: string = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=60';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  get apiListAllPokemons(): Observable<PokemonList> {
+    return this.http.get<PokemonList>(this.url).pipe(
+      tap(res => {
+        console.log('Resultado da API List All Pokemons:', res);
+      }),
+      tap( res => {
+        res.results.map( (resPokemons: Pokemon) => {
+          this.apiGetPokemon(resPokemons.url).subscribe(
+            details => {
+              console.log(`Detalhes do Pokémon ${resPokemons.name}:`, details);
+              resPokemons.details = details 
+            }
+          );
+        })
+      })
+    )
+  }
+
+  public apiGetPokemon( url: string ): Observable<PokemonDetails> {
+    return this.http.get<PokemonDetails>( url ).pipe(
+      map(
+        res => res
+      )
+    )
+  }
+}
