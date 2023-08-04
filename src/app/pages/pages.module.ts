9b8ee1de2d63cdb440a57a83c1b1192ee024a8ac
zrodrigolimaz <zrodrigolimaz@gmail.com>
2023-08-04 00:48:16 -0300

    Implement lazy loading for Detailsmodule

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-// Modulo de roteamento
 import { RoutingModule } from './routing.module';
-
-//modules
 import { ReusableModule } from '../reusable/reusable.module';
 
-// Paginas
 import { HomeComponent } from './home/home.component';
-import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
-    DetailsComponent,
   ],
   imports: [
     CommonModule,
--- src/app/pages/routing.module.ts
+++ src/app/pages/routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-
 import { HomeComponent } from './home/home.component';
-import { DetailsComponent } from './details/details.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'details/:id', component: DetailsComponent }
+  { path: 'details', loadChildren: () => import('./details/details.module').then(m => m.DetailsModule) }
 ];
 
 @NgModule({
